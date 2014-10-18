223eed171155f95e676f50c14f32fbacd4d318fe
Gavin Brewer <gavin.brewer@capula.co.uk>
2014-10-18 17:31:30 +0100

    Prototype skeleton for key-management

---
+++ CryptoManager/crypto.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptoManager
+{
+    public class CryptoEngine
+    {
+        public CryptoEngine()
+        { }
+
+        public static string GenerateKeyPair(string targetFile)
+        {
+            RSACryptoServiceProvider algorithm = new RSACryptoServiceProvider();
+
+            //Save the private key
+            string completeKey = algorithm.ToXmlString(true);
+            byte[] keyBytes = Encoding.UTF8.GetBytes(completeKey);
+
+            keyBytes = ProtectedData.Protect(keyBytes, null, DataProtectionScope.CurrentUser);
+            
+            using (FileStream fs = new FileStream(targetFile, FileMode.Create))
+            {
+                fs.Write(keyBytes, 0, keyBytes.Length);
+            }
+
+            //return the public key
+            return algorithm.ToXmlString(false);
+        }
+    }
+}
